9d095eeeab90e31fb3024c92e370b5eb8b4a0cde
Anton Nazarov <anton_nazarov@hotmail.com>
2019-06-22 16:33:51 +0200

    The app packages were updated including script for Linux

--- apricot-distr/apricotdb-x.xx-all-bin/apricotdb.bat
+++
@@ -1,4 +0,0 @@
-java -jar apricot-launcher-0.1.jar
-if errorlevel 1 goto fail
-java -jar apricot-ui-0.1.jar
-:fail
--- apricot-distr/apricotdb-x.xx-jre-win64-bin/apricotdb.bat
+++ apricot-distr/apricotdb-x.xx-jre-win64-bin/apricotdb.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=.\jre1.8.0_202
-.\jre1.8.0_202\bin\java -jar apricot-launcher-0.4.jar
+.\jre1.8.0_202\bin\java -jar apricot-launcher-0.5.jar
 if errorlevel 1 goto fail
-.\jre1.8.0_202\bin\java -Xms1024m -Xmx2048m -jar apricot-ui-0.4.jar
+.\jre1.8.0_202\bin\java -Xms1024m -Xmx2048m -jar apricot-ui-0.5.jar
 :fail
