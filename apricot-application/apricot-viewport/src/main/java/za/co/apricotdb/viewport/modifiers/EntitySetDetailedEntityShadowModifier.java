542fc8113821158bcaf48ddc6cb881a42a79b36d
Anton Nazarov <anton_nazarov@hotmail.com>
2019-12-28 12:57:33 +0200

    The shadow modifier adapted for the coloring. Will test it in the next developmengt session

--- apricot-application/apricot-viewport/src/main/java/za/co/apricotdb/viewport/modifiers/EntitySetDetailedEntityShadowModifier.java
+++ apricot-application/apricot-viewport/src/main/java/za/co/apricotdb/viewport/modifiers/EntitySetDetailedEntityShadowModifier.java
@@ -4,7 +4,9 @@ import javafx.scene.Node;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import za.co.apricotdb.viewport.canvas.ApricotShape;
+import za.co.apricotdb.viewport.canvas.ElementStatus;
 import za.co.apricotdb.viewport.entity.shape.ApricotEntityShape;
 
 public class EntitySetDetailedEntityShadowModifier implements ElementVisualModifier {
@@ -13,7 +15,7 @@ public class EntitySetDetailedEntityShadowModifier implements ElementVisualModif
     public void modify(ApricotShape shape) {
         if (shape instanceof ApricotEntityShape) {
             VBox box = (VBox) shape;
-            DropShadow shadow = buildShadow();
+            DropShadow shadow = buildShadow(shape);
             for (Node n : box.getChildren()) {
                 if (n instanceof GridPane) {
                     GridPane gp = (GridPane) n;
@@ -23,7 +25,7 @@ public class EntitySetDetailedEntityShadowModifier implements ElementVisualModif
         }
     }
 
-    private DropShadow buildShadow() {
+    private DropShadow buildShadow(ApricotShape shape) {
         DropShadow shadow = new DropShadow();
         shadow.setWidth(3);
         shadow.setHeight(3);
@@ -31,6 +33,21 @@ public class EntitySetDetailedEntityShadowModifier implements ElementVisualModif
         shadow.setOffsetY(3);
         shadow.setRadius(4);
 
+        switch (getEntityStatus(shape)) {
+        case GRAYED:
+            shadow.setColor(Color.LIGHTGRAY);
+            break;
+        default:
+            shadow.setColor(Color.BLACK);
+            break;
+        }
+
         return shadow;
+    }
+
+    private ElementStatus getEntityStatus(ApricotShape shape) {
+        ApricotEntityShape entityShape = (ApricotEntityShape) shape;
+
+        return entityShape.getElement().getElementStatus();
     }
 }
