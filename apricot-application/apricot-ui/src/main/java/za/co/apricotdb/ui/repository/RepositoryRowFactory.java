2884a9faf5997977edced097591504da43fa5586
AntonNazarov <anton_nazarov@hotmail.com>
2020-04-19 20:52:32 +0200

    Row Factory added

--- apricot-application/apricot-ui/src/main/java/za/co/apricotdb/ui/repository/RepositoryRowFactory.java
+++ apricot-application/apricot-ui/src/main/java/za/co/apricotdb/ui/repository/RepositoryRowFactory.java
@@ -1,5 +1,18 @@
 package za.co.apricotdb.ui.repository;
 
+import org.springframework.stereotype.Component;
+
+/**
+ * This is the convenient factory to build the RepositoryRow holder.
+ * 
+ * @author Anton Nazarov
+ * @since 19/04/2020
+ */
+@Component
 public class RepositoryRowFactory {
+    
+    public RepositoryRow buildRow(RepositoryRowFactory) {
+        RepositoryRow 
+    }
 
 }
