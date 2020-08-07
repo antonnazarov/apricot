2e7068625c17db32657c92bb7ee83f439cb238a3
Anton Nazarov <anton_nazarov@hotmail.com>
2020-08-07 22:29:30 +0200

    Preparations for the refactoring of the Apricot dialog forms; Upgrade of the essential libraries

--- apricot-application/apricot-ui/src/main/java/za/co/apricotdb/ui/handler/ApricotAboutHandler.java
+++ apricot-application/apricot-ui/src/main/java/za/co/apricotdb/ui/handler/ApricotAboutHandler.java
@@ -1,13 +1,9 @@
 package za.co.apricotdb.ui.handler;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-import za.co.apricotdb.ui.util.ImageHelper;
+import za.co.apricotdb.ui.model.ApricotForm;
 
 import javax.annotation.Resource;
 import java.io.IOException;
@@ -24,17 +20,12 @@ public class ApricotAboutHandler {
     @Resource
     ApplicationContext context;
 
-    public void showAboutForm() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/za/co/apricotdb/ui/apricot-about-form.fxml"));
-        loader.setControllerFactory(context::getBean);
-        Pane window = loader.load();
+    @Autowired
+    DialogFormHandler formHandler;
 
-        final Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.setTitle("About Apricot DB");
-        Scene scene = new Scene(window);
-        dialog.setScene(scene);
-        dialog.getIcons().add(ImageHelper.getImage("favicon-32x32.png", getClass()));
-        dialog.show();
+    public void showAboutForm() throws IOException {
+        ApricotForm form = formHandler.buildApricotForm("/za/co/apricotdb/ui/apricot-about-form.fxml",
+                "favicon-32x32.png", "About Apricot DB");
+        form.show();
     }
 }
---
+++ apricot-application/apricot-ui/src/main/java/za/co/apricotdb/ui/model/ApricotForm.java
@@ -0,0 +1,29 @@
+package za.co.apricotdb.ui.model;
+
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+/**
+ * A DTO for the unified Apricot dialog (modal) form.
+ *
+ * @author Anton Nazarov
+ * @since 07/08/2020
+ */
+public class ApricotForm {
+
+    private FXMLLoader loader;
+    private Stage dialog;
+
+    public ApricotForm(FXMLLoader loader, Stage dialog) {
+        this.loader = loader;
+        this.dialog = dialog;
+    }
+
+    public <T> T getController() {
+        return loader.getController();
+    }
+
+    public void show() {
+        dialog.show();
+    }
+}
