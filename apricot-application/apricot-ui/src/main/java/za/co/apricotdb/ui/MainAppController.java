7f6f5c5a85876fddb2a6fb7e32d8e83105d5445d
Anton Nazarov <anton_nazarov@hotmail.com>
2020-09-04 13:51:14 +0200

    Added itext7 lib to the project

--- apricot-application/apricot-ui/src/main/java/za/co/apricotdb/ui/MainAppController.java
+++ apricot-application/apricot-ui/src/main/java/za/co/apricotdb/ui/MainAppController.java
@@ -37,6 +37,7 @@ import za.co.apricotdb.ui.handler.ExportProjectHandler;
 import za.co.apricotdb.ui.handler.GenerateScriptHandler;
 import za.co.apricotdb.ui.handler.ImportProjectHandler;
 import za.co.apricotdb.ui.handler.NonTransactionalPort;
+import za.co.apricotdb.ui.handler.PrintCanvasHandler;
 import za.co.apricotdb.ui.handler.ProjectExplorerContextMenuHandler;
 import za.co.apricotdb.ui.handler.ProjectExplorerItem;
 import za.co.apricotdb.ui.handler.RepositoryHandler;
@@ -159,6 +160,9 @@ public class MainAppController {
 
     @Autowired
     TbQuickViewHandler tbQuickViewHandler;
+
+    @Autowired
+    PrintCanvasHandler printCanvasHandler;
 
     @FXML
     AnchorPane mainPane;
@@ -636,6 +640,11 @@ public class MainAppController {
     public void filterReset(ActionEvent event) {
         filterHandler.resetEntityFilter();
         filterField.setText("*");
+    }
+
+    @FXML
+    public void printCanvas() {
+        printCanvasHandler.printCurrentCanvas();
     }
 
     public TextField getFilterField() {
